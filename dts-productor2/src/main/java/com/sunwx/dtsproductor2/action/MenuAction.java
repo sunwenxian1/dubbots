88131d0bdc8a4ede8429c9c0095861c6f3b17114
sun-glb <sun@qq.com>
2019-11-15 09:34:23 +0800

    share praject！

---
+++ dts-productor2/src/main/java/com/sunwx/dtsproductor2/action/MenuAction.java
@@ -0,0 +1,15 @@
+package com.sunwx.dtsproductor2.action;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.sunwx.dtscommon.interfaces.MenuInterface;
+import com.sunwx.dtscommon.interfaces.TestInterface;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+@Controller
+public class MenuAction {
+
+
+}
