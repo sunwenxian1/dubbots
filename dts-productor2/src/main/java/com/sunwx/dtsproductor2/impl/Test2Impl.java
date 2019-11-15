9d195519284d91a8d99204ec08f65964085cf746
sun-glb <sun@qq.com>
2019-11-15 09:35:28 +0800

    share praject！

---
+++ dts-productor2/src/main/java/com/sunwx/dtsproductor2/impl/Test2Impl.java
@@ -0,0 +1,15 @@
+package com.sunwx.dtsproductor2.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.sunwx.dtscommon.interfaces.Test2Interface;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test2Impl implements Test2Interface {
+
+    @Override
+    public List<String> getString() {
+        return null;
+    }
+}
